b639542e8c2f0398f97c524e8ab5d7212328fe97
Павел Наумов <pavlik33546@gmail.com>
2025-03-07 09:52:39 +0500

    Add project files.

---
+++ F# (Lab-2) - 2/2.fs
@@ -0,0 +1,18 @@
+﻿// Функция для подсчета строк заданной длины
+let countStringsOfLength (length: int) (strings: string list) =
+    List.fold (fun count str -> 
+        if String.length str = length then count + 1
+        else count
+    ) 0 strings
+
+// Исходный список строк
+let strings = ["Всем"; "привет!"; "Если"; "жизнь"; "трудна"; "сделай"; "её"; "проще"]
+
+// Заданная длина строки
+let targetLength = 5
+
+// Подсчитываем количество строк заданной длины
+let result = countStringsOfLength targetLength strings
+
+// Выводим результат
+printfn "Количество строк длины %d: %d" targetLength result
---
+++ F# (Lab-2)/1.fs
@@ -0,0 +1,14 @@
+﻿// Определяем функцию, которая добавляет символ в начало строки
+let prependChar (ch: char) (str: string) = string ch + str
+
+// Исходный список строк
+let strings = ["apple"; "banana"; "cherry"]
+
+// Заданный символ, который нужно добавить в начало каждой строки
+let symbol = '@'
+
+// Применяем List.map для создания нового списка
+let newStrings = List.map (prependChar symbol) strings
+
+// Выводим результат
+printfn "%A" newStrings
